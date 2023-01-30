2513124c83203e9390663e4294cdd81b1e19dacd
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-30 10:24:10 -0500

    add product service

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import {
+  addDoc,
+  collection,
+  collectionData,
+  Firestore,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductoService {
+  constructor(private firestore: Firestore) {}
 
-  constructor() { }
+  addProduct(producto: Producto) {
+    const productoRef = collection(this.firestore, 'productos');
+    return addDoc(productoRef, producto);
+  }
+
+  getProductos(): Observable<Producto[]> {
+    const productoRef = collection(this.firestore, 'productos');
+    return collectionData(productoRef, { idField: 'id' }) as Observable<
+      Producto[]
+    >;
+  }
+
+  deleteProducto(producto: Producto) {
+    const productoRef = doc(this.firestore, `productos/${producto.id}`);
+    return deleteDoc(productoRef);
+  }
+
+  updateProducto(id: string, producto: Producto) {
+    const productoRef = doc(this.firestore, `productos/${id}`);
+    return updateDoc(productoRef, { ...producto });
+  }
 }
