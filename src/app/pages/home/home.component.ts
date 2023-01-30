30a538ebf6bb58d2e415e5ddac099be248f856e5
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-30 09:50:49 -0500

    add home component

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from 'src/app/components/dialog/dialog.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(DialogComponent, { disableClose: true });
+  }
+}
